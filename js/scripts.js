101288f8453028a43ed6a1194bfcf2ce85f94169
Barbara Śmiech <b.smiech@wp.pl>
2017-09-05 16:45:30 +0200

    wrong

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-var list = document.getElementById('list')
+var list = document.getElementById('list');
+var add = document.getElementById('button');
+
+buttonElem.addEventListener('click', function(
+    var element = document.createElement('li');
+    element.innerHTML = 'item';) {
+  document.getElementsByTagName('item')
+  list.appendChild(element); 
+});
